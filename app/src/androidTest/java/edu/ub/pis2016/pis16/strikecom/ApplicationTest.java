78cf2a6c39adfa8bfaddf65242c0213e165c0525
Herman Dempere <nerucius@gmail.com>
2016-03-08 09:15:56 +0100

    Initial commit

---
+++ app/src/androidTest/java/edu/ub/pis2016/pis16/strikecom/ApplicationTest.java
@@ -0,0 +1,13 @@
+package edu.ub.pis2016.pis16.strikecom;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+	public ApplicationTest() {
+		super(Application.class);
+	}
+}
---
+++ app/src/main/java/edu/ub/pis2016/pis16/strikecom/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.ub.pis2016.pis16.strikecom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
