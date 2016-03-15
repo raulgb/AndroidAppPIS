16f79f158050ff42fc33c3f37ac8533f3ce0ea58
Herman Dempere <nerucius@gmail.com>
2016-03-15 09:50:49 +0100

    Deprecated Canvas

--- app/src/main/java/edu/ub/pis2016/pis16/strikecom/Canvas2DGame.java
+++ app/src/main/java/edu/ub/pis2016/pis16/strikecom/Canvas2DGame.java
@@ -5,6 +5,7 @@ import edu.ub.pis2016.pis16.strikecom.engine.android.AndroidGame;
 import edu.ub.pis2016.pis16.strikecom.engine.framework.Screen;
 import edu.ub.pis2016.pis16.strikecom.screens.Test2DScreen;
 
+@Deprecated
 public class Canvas2DGame extends AndroidGame {
 	@Override
 	public Screen getStartScreen() {
