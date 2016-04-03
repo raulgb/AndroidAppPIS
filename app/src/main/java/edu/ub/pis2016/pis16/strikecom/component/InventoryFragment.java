859b69f8fbcf233db7bc3bd8691129ce96658be5
Herman Dempere <nerucius@gmail.com>
2016-04-03 19:20:29 +0200

    - Added layout to InventoryFragment and linked it

--- app/src/main/java/edu/ub/pis2016/pis16/strikecom/component/InventoryFragment.java
+++ app/src/main/java/edu/ub/pis2016/pis16/strikecom/component/InventoryFragment.java
@@ -1,6 +1,17 @@
 package edu.ub.pis2016.pis16.strikecom.component;
 
-import android.app.Fragment;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class InventoryFragment extends Fragment {
+import edu.ub.pis2016.pis16.strikecom.R;
+
+public class InventoryFragment extends DialogFragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment_inventory, container, false);
+	}
 }
